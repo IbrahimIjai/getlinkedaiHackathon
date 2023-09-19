e724cda1c911f8395b4d7858031cc89b8acd8116
Steve Tomi <89354038+SteffQing@users.noreply.github.com>
2023-09-19 17:19:14 +0100

    register opening

---
+++ src/app/register/page.tsx
@@ -0,0 +1,26 @@
+import StarsBackground from "@/components/Stars";
+import { MovementFemale, MovementMale } from "./svg";
+import OnboardingPerson from "./images/OnboardingPerson.png";
+import Text from "@/components/Text";
+import Image from "next/image";
+export default function Register() {
+  return (
+    <main className="flareBg bg-secondary">
+      <StarsBackground />
+      <section>
+        <aside className="relative">
+          <Image src={OnboardingPerson} alt="Onboarding Person icon" />
+        </aside>
+        <aside>
+          <div className="flex gap-2">
+            <Text text="Be part of this movement!" isWhite />
+            <div className="relative w-[90px]">
+              <MovementFemale className="absolute" />
+              <MovementMale className="absolute" />
+            </div>
+          </div>
+        </aside>
+      </section>
+    </main>
+  );
+}
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,8 +2,9 @@ interface Props {
   text: string;
   isWhite?: boolean;
   type?: "h1" | "h3" | "p";
+  className?: string;
 }
-export default function Text({ text, isWhite, type }: Props) {
+export default function Text({ text, isWhite, type, className }: Props) {
   switch (type) {
     case "h1":
       return <H1 text={text} isWhite={isWhite} />;
@@ -15,32 +16,34 @@ export default function Text({ text, isWhite, type }: Props) {
       return <P text={text} isWhite={isWhite} />;
   }
 }
-function H1({ text, isWhite }: Props) {
+function H1({ text, isWhite, className }: Props) {
   return (
     <h1
       className={`text-3xl font-bold ${
         isWhite ? "text-white" : "text-primary"
-      }`}
+      } ${className ? className : ""}`}
     >
       {text}
     </h1>
   );
 }
-function H3({ text, isWhite }: Props) {
+function H3({ text, isWhite, className }: Props) {
   return (
     <h3
-      className={`text-xl font-bold ${isWhite ? "text-white" : "text-primary"}`}
+      className={`text-xl font-bold ${
+        isWhite ? "text-white" : "text-primary"
+      } ${className ? className : ""}`}
     >
       {text}
     </h3>
   );
 }
-function P({ text, isWhite }: Props) {
+function P({ text, isWhite, className }: Props) {
   return (
     <p
-      className={`text-base font-bold ${
-        isWhite ? "text-white" : "text-primary"
-      }`}
+      className={`text-base font-bold ${isWhite ? "text-white" : "text-primary"}
+      ${className ? className : ""}
+      `}
     >
       {text}
     </p>
